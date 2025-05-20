2dd4d55745607ea2780ce7c0af7891e399acdc6f
Alexandria Wright <145223522+alexw2326@users.noreply.github.com>
2025-05-20 14:40:36 -0400

    fixed edu pic

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -9,7 +9,7 @@ const Education = () => {
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex items-center justify-center">
-                        <img className="rounded-2xl max-w-full h-auto" src={GradPhoto} alt="Graduation" />
+                        <img className="rounded-2xl w-10/12 lg:w-full max-w-full h-auto" src={GradPhoto} alt="Graduation" />
                     </div>
                 </div>
                 <div className="w-full lg:w-1/2 lg:p-8 flex items-center">
